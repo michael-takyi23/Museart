677e55133ecdafef9b2950f69c441803d7ba2661
Michael Takyi <michaeltakyi11@gmail.com>
2024-06-17 16:03:25 +0000

    modify static files, css base.css

--- artimon/settings.py
+++ artimon/settings.py
@@ -148,11 +148,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
