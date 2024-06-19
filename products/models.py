3f6109698944e17a648c2a8f28410e45e8e9d6a5
Michael Takyi <michaeltakyi11@gmail.com>
2024-06-19 16:12:16 +0000

    Add products app, models and fixtures

---
+++ products/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Category
+# Product
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def _str_(self):
+        return self.name
+
+        def get_friendly_name(self):
+            return self.friendly_name
+
+class Product(models.Model):
+    category = models.ForeignKey('category', null=True, blank=True, on_delete=models.SET_NULL)
+    Sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, default=0, max_digits=6)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name            
