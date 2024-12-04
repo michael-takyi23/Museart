0b78c1fceca93ba86e5072afdfaae2c9fa302007
Michael Oppong Takyi <michaeltakyi11@gmail.com>
2024-12-04 12:59:07 +0000

    Modify settings.py, fix indentation issue

--- museart/settings.py
+++ museart/settings.py
@@ -198,27 +198,26 @@ if 'USE_AWS' in os.environ:
 
    AWS_S3_OBJECT_PARAMETERS = {
        'Expires' : 'Thu, 31 Dec 2029 20:00:00 GMT',
-       'CacheControl' : 'max-age=9460800',
-   }
+       'CacheControl' : 'max-age=9460800',}
 
-    #Bucket config
-    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME = 'eu-north-1'
-    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+   #Bucket config
+   AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+   AWS_S3_REGION_NAME = 'eu-north-1'
+   AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+   AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+   AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+   
+   # Static and media files
+   STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+   STATICFILES_LOCATION = 'static'
+   DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+   MEDIAFILES_LOCATION = 'media'
 
-    # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIAFILES_LOCATION = 'media'
+   # Override static and media URLs in production
+   STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+   MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
-    # Override static and media URLs in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
-# Default primary key field type
+   # Default primary key field type
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
