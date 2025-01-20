2e7753b343916ff7b80216945d1448bb563d447a
Michael Oppong Takyi <michaeltakyi11@gmail.com>
2025-01-20 15:23:47 +0000

    Update error templates, settings, and static files
    - Improved 404 and 500 error templates
    - Updated base CSS styling in static files
    - Adjusted settings.py for configuration changes
    - Removed unused media file: landing_page.jpg
    - Added new landing page image to static/images
    - Updated requirements.txt for dependency changes

--- museart/settings.py
+++ museart/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -184,7 +185,7 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
